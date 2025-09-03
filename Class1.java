f4bc61a23747a35f15a343bd66a6ad8b42119bcd
Omar Burton <omarburton@The8thDwarfs-MacbookAir.local>
2025-09-02 22:35:21 -0500

    Added f4()

--- Class1.java
+++ Class1.java
@@ -12,9 +12,12 @@ public class Class1 {
     private void f2() {
         // v2.0
     }
-    
+
     private void f3() {
         // v3.1
     }
     
+    private void f4() {
+        // v4.0
+    }
 }
